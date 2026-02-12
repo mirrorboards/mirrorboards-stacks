ac379f25070d95b2331f1cb30ba8237b33f6a3d4
lunacrafts <lunacrafts@protonmail.com>
2026-02-12 10:40:07 +0100

    Automated commit via save function

---
+++ core-xauth/core-xauth-db-postgres/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes"
+	"github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/apiextensions"
+	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/meta/v1"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		coreSystemStack, err := pulumi.NewStackReference(ctx, "mirrorboards/core-acta/dev", nil)
+		if err != nil {
+			return err
+		}
+		NamespaceName := coreSystemStack.GetStringOutput(pulumi.String("NamespaceName"))
+
+		PostgresCluster, err := apiextensions.NewCustomResource(ctx, "core-acta-postgres", &apiextensions.CustomResourceArgs{
+			ApiVersion: pulumi.String("postgresql.cnpg.io/v1"),
+			Kind:       pulumi.String("Cluster"),
+			Metadata: &metav1.ObjectMetaArgs{
+				Name:      pulumi.String("core-acta-postgres"),
+				Namespace: NamespaceName,
+			},
+			OtherFields: kubernetes.UntypedArgs{
+				"spec": pulumi.Map{
+					"instances": pulumi.Int(1),
+					"storage": pulumi.Map{
+						"size": pulumi.String("1Gi"),
+					},
+				},
+			},
+		})
+		if err != nil {
+			return err
+		}
+
+		PostgresSecretName := pulumi.String("core-acta-postgres-app")
+
+		ctx.Export("PostgresClusterName", PostgresCluster.Metadata.Name())
+		ctx.Export("PostgresSecretName", PostgresSecretName)
+
+		return nil
+	})
+}
